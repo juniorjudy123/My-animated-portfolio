869d4a28e048be0ff512c56845c6f6dcd049a134
benoy_Judy <benoyjudy@gmail.com>
2024-02-28 20:33:13 +0530

    first commit after cloning

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import "./navbar.scss"
+
+export default function Navbar() {
+	return (
+		<div className="navbar">
+			{/* sidebar*/}
+			<div className="wrapper">
+				<span>Benoy Judy</span>
+				<div className="social">
+					<a href="#">
+						<img src="/facebook.png"></img>
+					</a>
+					<a href="#">
+						<img src="/instagram.png"></img>
+					</a>
+					<a href="#">
+						<img src="/youtube.png"></img>
+					</a>
+					<a href="#">
+						<img src="/dribbble.png"></img>
+					</a>
+				</div>
+			</div>
+		</div>
+	)
+}
